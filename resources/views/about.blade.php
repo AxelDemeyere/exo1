c1c73afc5acbe4608e3edac763cd63240116b89b
AxelDemeyere <axel.demeyere59@gmail.com>
2025-02-17 14:22:38 +0100

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.header')
+@section('title', 'A Propos')
+
+@section('content')
+    <div>
+        <h1>A propos</h1>
+        <p>...</p>
+    </div>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.header')
+@section('title', 'Me contacter')
+
+@section('content')
+<div>
+    <h1>Me contacter</h1>
+    <p>...</p>
+</div>
+@endsection
